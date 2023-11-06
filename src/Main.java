7d7745159d234135463e209c3264065e72aac66f
KyleChan <kylec2026@student.cis.edu.hk>
2023-11-06 12:43:16 +0800

    ggs

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String one) {
+    int count = 0;
+        for (int i = 0; i < one.length(); i++) {
+        if (one.charAt(i) == 'a' || one.charAt(i) == 'e' || one.charAt(i) == 'i' || one.charAt(i) == 'o' || one.charAt(i) == 'u') {
+            count = count + 1;
+        }
+    }
+        return count;
+}
+
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,6 +23,16 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!(i%2==0) && !(i%3==0) && !(i%5==0)) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
 
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
@@ -24,6 +43,26 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static Object camelCaseMe(String four) {
+        String result = "";
+        if (four.charAt(0)=='-' || four.charAt(0)=='_') {
 
+        }
+        else {
+            result = result + four.charAt(0);
+        }
+        for (int i = 1; i < four.length(); i++) {
+            if (!(four.charAt(i)=='-' || four.charAt(i)=='_') && (four.charAt(i-1)=='-' || four.charAt(i-1)=='_')) {
+                result = result + (four.charAt(i)+"").toUpperCase();
+            }
+            else if ((four.charAt(i)=='-' || four.charAt(i)=='_')) {
+
+            }
+            else {
+                result = result + four.charAt(i);
+            }
+        }
+        return result;
+    }
 
 }
